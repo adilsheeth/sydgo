fb9b5baadcb1bc9c11eca3534fc44184f6df4f90
Adil <adilsheeth21@gmail.com>
2022-10-12 20:49:45 +1100

    Added bottom tab navigation and basic

---
+++ screens/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+class Home extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <Text>Home</Text>
+        );
+    }
+}
+ 
+export default Home;
---
+++ screens/Updates.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+class Updates extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <Text>Updates</Text>
+        );
+    }
+}
+ 
+export default Updates;
